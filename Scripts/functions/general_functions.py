8c5818955cbe50766f5e05e653b028dd030e876d
pedro-vr <pedro_vela_07@hotmail.com>
2024-06-10 22:56:01 -0600

    Exploración de loa datos

--- Scripts/functions/general_functions.py
+++ Scripts/functions/general_functions.py
@@ -23,3 +23,46 @@ def get_data_path(type_of_data):
 		data_path = 'Tipo de data no existente'
 
 	return data_path
+
+#Función para crear los histogramas de cada columna del conjunto de datos de prueba (el de entrenamiento contiene 
+#las mismas columnas + las variables a predecir que no nos interesa incluir en este análisis)
+def plot_hist_columns(df,from_column,to_column,num_rows_ax,num_cols_ax):
+
+    #Librerias a utilizar
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    #Creamos la lista con los nombres de columnas a plotear
+    columnas_rec = list(df.columns)[from_column:to_column]
+
+    #Creamos la plantilla para una gráfica del tamaño requerido
+    fig, axes = plt.subplots(nrows=num_rows_ax, ncols=num_cols_ax, figsize=(10, 10))
+
+    #Aplanamos los ejes para poder iterar sobre ellos más fácil
+    axes = axes.flatten()
+
+    #Lista para definir el número de columna que estamos graficando
+    list_num_col = list(range(from_column,to_column))
+
+    #Hacemos el loop sobre cada columna para hacer su gráfica
+    for i,column in enumerate(columnas_rec):
+        
+        #Creamos el histograma de la columna correspondiente
+        df[column].hist(ax=axes[i], #Definimos el eje sobre el cual estamos trabajando
+                        edgecolor='white', #Color del borde
+                        color='#69b3a2' #Color de los intervalos
+                       )
+
+        #Obtenemos el nombre de la columna correspondiente
+        col_name = list_num_col[i]
+    
+        #Agregamos los títulos a las gráficas
+        axes[i].set_title(f'COL_{col_name} dist.') 
+        axes[i].set_xlabel(f'COL_{col_name}') 
+        axes[i].set_ylabel('Frequency') 
+
+    #Ajustamos la gráfica
+    plt.tight_layout()
+
+    #Mostramos la gráfica final
+    plt.show()
